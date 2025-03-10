6b66f24f1ff6ca875524891f52bc495f6e47d808
Cosmin <timoftecosmin@yahoo.com>
2025-03-10 17:06:35 +0200

    Add Home page components

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+
+const hero = () => {
+  return (
+    <div>
+      <p>This is the hero</p>
+    </div>
+  )
+}
+
+export default hero
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.css'
+import Hero from '../Components/Hero/Hero'
+import Work from '../Components/Work/Work'
+import About from '../Components/About/About'
 
 const Home = () => {
   return (
-    <div>
-      <p>This is my protfoloio</p>
+    <div className="home">
+      <Hero/>
+      <Work/>
+      <About/>
     </div>
   )
 }
