a77f3eb6264491642dd76232ee62adc064d88a02
Cosmin <timoftecosmin@yahoo.com>
2025-03-10 17:47:33 +0200

    Add header structure

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,18 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="header">
+      <div className="logo">
+        <p>COSMIN TIMOFTE</p>
+      </div>
+      <nav className="nav" aria-label="Main Navigation">
+        <ul>
+          <li><a href="#about">Work</a></li>
+          <li><a href="#projects">About</a></li>
+          <li><a href="#contact">Download my CV</a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
