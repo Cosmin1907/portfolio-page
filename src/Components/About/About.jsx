28cc61a792935990bd19eca1c89b37787546b620
Cosmin <timoftecosmin@yahoo.com>
2025-03-10 18:45:21 +0200

    Add about structure

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,9 +3,26 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="about-container">
+      <div className="about-text">
+        <h2>ABOUT ME</h2>
+        <h3>I am a full stack developer based in Romania.</h3>
+        <p>A Romania based full-stack developer passionate about building accessible and user friendly web apps.</p>
+      </div>
+      <div className="about-skills">
+        <h3>The skills, tools and technologies I am good at:</h3>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Git</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
