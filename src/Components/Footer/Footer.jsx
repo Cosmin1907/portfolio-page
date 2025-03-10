409934bf0ac63f3ae2bf1113c5b605021ac8b879
Cosmin <timoftecosmin@yahoo.com>
2025-03-10 18:02:00 +0200

    Add icons and footer structure

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,9 +3,17 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='footer'>
+      <div className="back-link">
+        <a href="#header">Back to top</a>
+      </div>
+      <div className="social-links">
+        <a href="http://instagram.com" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+        <a href="http://x.com" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="http://github.com" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+        <a href="mailto:cosmintimofte@email.com" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-square-envelope"></i></a>
+      </div>
+    </footer>
   )
 }
 
