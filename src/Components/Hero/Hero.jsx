bc39168af53941467790c52baa4300873eff2117
Cosmin <timoftecosmin@yahoo.com>
2025-03-10 18:25:27 +0200

    Add hero strucutre

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,8 +3,23 @@ import './Hero.css'
 
 const hero = () => {
   return (
-    <div>
-      <p>This is the hero</p>
+    <div className="hero">
+      <div className="hero-left">
+        <h1>HI, I AM COSMIN TIMOFTE</h1>
+        <p>A Romania based full-stack developer passionate about building accessible user friendly apps.</p>
+        <div className="social-links">
+          <p>GET IN TOUCH:</p>
+          <a href="http://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label='LinkedIn'><i class="fa-brands fa-linkedin"></i></a>
+          <a href="mailto:cosmintimfote@email.com" target="_blank" rel="noopener noreferrer"><i class="fa-regular fa-envelope"></i></a>
+        </div>
+        <div className="github">
+          <p>SEARCH MY GITHUB:</p>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src="" alt="my photo" />
+      </div>
     </div>
   )
 }
