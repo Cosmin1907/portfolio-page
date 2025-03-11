0ae40ab90f609a3a86bccb69a03676c2e8ef2eef
Cosmin <timoftecosmin@yahoo.com>
2025-03-11 08:45:10 +0200

    Add project card structure

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -3,8 +3,20 @@ import './ProjectCard.css'
 
 const ProjectCard = () => {
   return (
-    <div>
-      
+    <div className="project-card">
+      <div className="project-card-image">
+        <img src="https://via.placeholder.com/150" alt="project" />
+      </div>
+      <div className="project-card-content">
+        <h3>Project Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <h4>Project info:</h4>
+        <p>Year: </p>
+        <p>Technologies: </p>
+        <Button/>
+      </div>
     </div>
   )
 }
--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Work.css'
+import Card from '../ProjectCard/ProjectCard'
 
 const Work = () => {
   return (
