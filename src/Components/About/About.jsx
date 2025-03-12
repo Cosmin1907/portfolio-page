8b8e93ff24e56b5808ffa6e18c45bc6f3682bc41
Cosmin <timoftecosmin@yahoo.com>
2025-03-12 12:28:07 +0200

    Add fontawesome to the skills

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,16 +12,17 @@ const About = () => {
       <div className="about-skills">
         <h3>The skills, tools and technologies I am good at:</h3>
         <ul>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>JavaScript</li>
-          <li>Python</li>
-          <li>React</li>
-          <li>Django</li>
-          <li>Flask</li>
-          <li>Git</li>
+          <li><i className="fa-brands fa-html5" style={{ color: '#E34F26' }}></i> HTML5</li>
+          <li><i className="fa-brands fa-css3-alt" style={{ color: '#1572B6' }}></i> CSS3</li>
+          <li><i className="fa-brands fa-js" style={{ color: '#F7DF1E' }}></i> JavaScript</li>
+          <li><i className="fa-brands fa-python" style={{ color: '#3776AB' }}></i> Python</li>
+          <li><i className="fa-brands fa-react" style={{ color: '#61DAFB' }}></i> React</li>
+          <li><i className="fa-brands fa-django" style={{ color: '#092E20' }}></i> Django</li>
+          <li><i className="fa-brands fa-flask" style={{ color: '#000000' }}></i> Flask</li>
+          <li><i className="fa-brands fa-git-alt" style={{ color: '#F05032' }}></i> Git</li>
         </ul>
       </div>
+
     </section>
   )
 }
