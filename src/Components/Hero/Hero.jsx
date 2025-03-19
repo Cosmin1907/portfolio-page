2729cccf5e19d09155663bc0017219b9b51127db
Cosmin <timoftecosmin@yahoo.com>
2025-03-19 12:24:16 +0200

    Add dual language to Hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Hero.css'
 import GitHub from '../GitHubProfile/GitHubProfile'
+import { useTranslation } from 'react-i18next'
 
 const Hero = () => {
+  const { t } = useTranslation();
   return (
     <section className="hero">
       <div className="hero-left">
-        <h1>HI, I AM <br />COSMIN TIMOFTE</h1>
-        <p>A Romania based full-stack developer passionate about building accessible user friendly apps.</p>
+        <h1>{t('h1Label')}<br />{t('nameLabel')}</h1>
+        <p>{t('herodescription')}</p>
         <div className="social-links">
-          <p>GET IN TOUCH:</p>
+          <p>{t('contact')}:</p>
           <a href="http://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label='LinkedIn'><i class="fa-brands fa-linkedin"></i></a>
           <a href="mailto:cosmintimfote@email.com" target="_blank" rel="noopener noreferrer"><i class="fa-regular fa-envelope"></i></a>
         </div>
